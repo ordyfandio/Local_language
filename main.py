5eab1695405eaf528ad8e4e7b56a18367b052bce
Mordy fandio <ordybeni@gmail.com>
2024-06-03 23:38:05 +0100

    Finir bientot avec les pages introduction l'app

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from kivy.app import App
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.lang.builder import Builder
 from kivy.core.window import Window
+from kivy.base import runTouchApp
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.image import Image
+from kivy.uix.button import Button
 Window.clearcolor = 1,1,1,1
 
 class WindowManager(ScreenManager):
@@ -9,20 +13,32 @@ class WindowManager(ScreenManager):
 
 class HeroScreen(Screen):
     pass
-        
-
 class FrontPageScreen(Screen):
     pass
 
 class IntroScreen(Screen):
     pass
 
+class profileScreen(Screen):
+    pass
+
+class NoAboutScreen(Screen):
+    def change(self):
+        HB=BoxLayout(orientation="horizontal")
+        
+        img=Image(source="family.jpg",size_hint=(0.2, 0.2))
+        btn1=Button(text="Family/Friend",size_hint=(None,None),width="100px",height="70px")
+        HB.add_widget(img)
+        HB.add_widget(btn1)
 class Speakeasy(App):
     def build(self):
         root = WindowManager()
         root.add_widget(HeroScreen(name='Hero'))
         root.add_widget(FrontPageScreen(name='FrontPage'))
-        root.add_widget(IntroScreen(name='Intro'))
+        root.add_widget(profileScreen(name='Profile'))
+        root.add_widget(NoAboutScreen(name='NoAbout'))
+        runTouchApp(root.add_widget(IntroScreen(name='Intro')))
+        self.icon="logo.jpeg"
         return root
 
 if __name__ == '__main__':
